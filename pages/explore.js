bf780b1989a87ac864621f99f515f5aa0afa63da
Adil Rahman <itsadil2022@gmail.com>
2025-02-26 20:00:01 +0600

    Added card and gave it a simple style

--- pages/explore.js
+++ pages/explore.js
@@ -4,23 +4,31 @@ export default async function Explore() {
   const cats = data.cats;
   console.log(typeof cats);
   console.log(cats);
-  let content = `<h1>Explore Page</h1><p>Discover amazing things here!</p>`;
+  let content = `
+      <h1>Explore Page</h1>
+      <p>Discover amazing things here!</p>
+      <div class="card-container">
+  `;
 
   if (cats.length > 0) {
     cats.forEach((cat) => {
       content += `
-                <div>
-                    <h2>${cat.cat_name}</h2>
-                    <img src="${cat.cat_image}" alt="${cat.cat_name}" width="200" />
-                    <p>Age: ${cat.cat_age}</p>
-                    <p>Gender: ${cat.cat_gender}</p>
-                    <p>${cat.cat_description}</p>
-                </div>
-            `;
+          <div class="card">
+              <img src="${cat.cat_image} alt="${cat.cat_name}" />
+              <div class="card-text">
+                <h2>${cat.cat_name}</h2>
+                <p>Age: ${cat.cat_age}</p>
+                <p>Gender: ${cat.cat_gender}</p>
+                <p>${cat.cat_description}</p>
+              </div>
+          </div>
+      `;
     });
   } else {
     content += `<p>No cats available right now.</p>`;
   }
+
+  content += `</div>`; 
 
   return content;
 }
