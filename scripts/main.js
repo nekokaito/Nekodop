7f403dbaa5c9f7232636ba859ffb6ed5ff314b16
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2025-03-05 11:20:30 +0600

    Protected Routes Setup

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,24 @@
-
-
 document.addEventListener("DOMContentLoaded", () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const currentPath = window.location.pathname;
+
+  const loginPages = ["/pages/login.html", "/pages/sign-up.html"];
+
+  // Protected Routes
+  if (user && loginPages.includes(currentPath)) {
+    window.location = "/";
+    console.log("Already Has Been Logged In");
+  }
+
   const showActiveSection = () => {
     const sections = document.querySelectorAll("#app > div");
     const hash = window.location.hash || "#home";
+
+    // Redirect to login if not authenticated
+    if (hash === "#profile" && !user) {
+      window.location = "../pages/login.html";
+      return;
+    }
 
     sections.forEach((section) => {
       section.style.display = `#${section.id}` === hash ? "block" : "none";
