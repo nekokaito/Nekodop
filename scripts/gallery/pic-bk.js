1e2da6ba1d7c1dde75c299d2fd9c69794fc22b0b
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2025-07-25 01:07:20 +0600

    Add gallery section with responsive layout and styles

--- scripts/gallery/pic-bk.js
+++ scripts/gallery/pic-bk.js
@@ -1,44 +1,33 @@
-
-export const galleryImages =() => {
+export const galleryImages = () => {
   console.log("Gallery Images Loaded");
-const images = [
-      "cat1.jpg",
-      "cat2.jpg",
-      "cat3.jpg",
-      "cat4.jpg",
-      "cat5.jpg",
-      "cat6.jpg",
-      "chill-mewoi.png",
-      "chill-mewoi.png",
-      "chill-mewoi.png",
-    ];
+  const images = [
+    "cat1.jpg",
+    "cat2.jpg",
+    "cat3.jpg",
+    "cat4.jpg",
+    "cat5.jpg",
+    "cat6.jpg",
+    "chill-mewoi.png",
+    "chill-mewoi.png",
+    "chill-mewoi.png",
+    "cat1.jpg",
+    "cat2.jpg",
+    "cat3.jpg",
+    "cat4.jpg",
+    "cat5.jpg",
+    "cat6.jpg"
+  ];
+  const galleryContainer = document.querySelector(".gallery-container");
+  images.forEach((image) => {
+    const imgdiv = document.createElement("div");
+    imgdiv.classList.add("gallery-item");
+    const imgElement = document.createElement("img");
 
-    const columns = 3;
-    const table = document.getElementById("galleryTable");
-
-    if (table) {
-      for (let i = 0; i < images.length; i += columns) {
-        const row = document.createElement("tr");
-
-        for (let j = 0; j < columns; j++) {
-          const cell = document.createElement("td");
-          const imgIndex = i + j;
-
-          if (imgIndex < images.length) {
-            const img = document.createElement("img");
-            img.src = "/images/for-gallery/" + images[imgIndex];  // Make sure this path is correct!
-            img.width = 150;
-            img.height = 150;
-            img.alt = "Cat " + (imgIndex + 1);
-            cell.appendChild(img);
-          }
-
-          row.appendChild(cell);
-        }
-
-        table.appendChild(row);
-      }
-    } else {
-      console.error('Element with id "galleryTable" not found.');
-    }
-  }
+    imgElement.src = `/images/for-gallery/${image}`;
+    imgElement.alt = `Gallery Image ${image}`;
+    
+    imgdiv.appendChild(imgElement);
+    galleryContainer.appendChild(imgdiv);
+  });
+  console.log("Gallery Images Appended");
+};
