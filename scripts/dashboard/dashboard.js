52967698c675913d104d5ec5c1ddbf7f73a9b9b2
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2025-07-25 11:54:58 +0600

    Refactor user details handling in dashboard; move user info updates to fetchUsers function

--- scripts/dashboard/dashboard.js
+++ scripts/dashboard/dashboard.js
@@ -3,14 +3,9 @@ import { fetchPosts } from "./get-posts.js";
 import { fetchUsers } from "./get-users.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const user = JSON.parse(localStorage.getItem("user"));
-  const userName = user?.name || "Admin";
-  const profilePicture = user?.profilePicture || "../images/profile.png";
+  
 
   await fetchUsers();
   await fetchPosts();
   setupAdoptionActions();
-
-  document.getElementById("dashboard-username").textContent = userName;
-  document.getElementById("dashboard-profile-picture").src = profilePicture;
 });
--- scripts/dashboard/get-users.js
+++ scripts/dashboard/get-users.js
@@ -6,8 +6,19 @@ export const fetchUsers = async () => {
     const data = await res.json();
     const userList = data.users;
     renderUsers(userList);
+
+    // Update total users count
     const totalUsers = userList.length;
     document.getElementById("total-users").textContent = totalUsers;
+
+    // Update user details in the dashboard
+    const user = JSON.parse(localStorage.getItem("user"));
+    const userName = user?.name || "Admin";
+    const profilePicture = user?.profilePicture || "../images/profile.png";
+    document.getElementById("dashboard-username").textContent =
+      userName || "Admin";
+    document.getElementById("dashboard-profile-picture").src =
+      profilePicture || "../images/profile.png";
   } catch (error) {
     console.error("Error fetching users:", error);
   }
